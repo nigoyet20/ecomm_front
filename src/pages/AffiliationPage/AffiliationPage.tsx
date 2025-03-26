4027063e90c1ae447f7c5149c508cc98de62b984
Nicolas Goyet <nicolasgoyet20@gmail.com>
2025-03-26 20:26:10 +0100

    dqsd

--- src/pages/AffiliationPage/AffiliationPage.tsx
+++ src/pages/AffiliationPage/AffiliationPage.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import ButtonLoader from '../../components/App/ButtonLoader/ButtonLoader';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import { AiFillSignature } from "react-icons/ai";
